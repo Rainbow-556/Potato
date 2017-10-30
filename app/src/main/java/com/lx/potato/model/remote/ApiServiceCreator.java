438333ddf9cd456254b68f0dc93adf94b911e1bd
lixiang <903263494@qq.com>
2017-10-30 08:23:31 +0800

    1.添加解析get和post请求参数的方法

--- app/src/main/java/com/lx/potato/model/remote/ApiServiceCreator.java
+++ app/src/main/java/com/lx/potato/model/remote/ApiServiceCreator.java
@@ -4,9 +4,17 @@ import android.util.Log;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -42,5 +50,75 @@ public final class ApiServiceCreator {
                 .client(okHttpClient)
                 .build();
         return retrofit.create(ApiService.class);
+    }
+
+    /**
+     * 解析请求参数
+     *
+     * @param request
+     * @return
+     */
+    public static Map<String, String> parseParams(Request request) {
+        //GET POST DELETE PUT PATCH
+        String method = request.method();
+        Map<String, String> params = null;
+        if ("GET".equals(method)) {
+            params = doGet(request);
+        } else if ("POST".equals(method) || "PUT".equals(method) || "DELETE".equals(method) || "PATCH".equals(method)) {
+            RequestBody body = request.body();
+            if (body != null && body instanceof FormBody) {
+                params = doForm(request);
+            }
+        }
+        return params;
+    }
+
+    /**
+     * 获取get方式的请求参数
+     *
+     * @param request
+     * @return
+     */
+    private static Map<String, String> doGet(Request request) {
+        Map<String, String> params = null;
+        HttpUrl url = request.url();
+        Set<String> strings = url.queryParameterNames();
+        if (strings != null) {
+            Iterator<String> iterator = strings.iterator();
+            params = new HashMap<>();
+            int i = 0;
+            while (iterator.hasNext()) {
+                String name = iterator.next();
+                String value = url.queryParameterValue(i);
+                params.put(name, value);
+                i++;
+            }
+        }
+        return params;
+    }
+
+    /**
+     * 获取表单的请求参数
+     *
+     * @param request
+     * @return
+     */
+    private static Map<String, String> doForm(Request request) {
+        Map<String, String> params = null;
+        FormBody body = null;
+        try {
+            body = (FormBody) request.body();
+        } catch (ClassCastException c) {
+        }
+        if (body != null) {
+            int size = body.size();
+            if (size > 0) {
+                params = new HashMap<>();
+                for (int i = 0; i < size; i++) {
+                    params.put(body.name(i), body.value(i));
+                }
+            }
+        }
+        return params;
     }
 }
